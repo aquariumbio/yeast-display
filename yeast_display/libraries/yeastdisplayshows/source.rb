c52f28cd95ca248774c47997fa0839a911329fb2
Devin Strickland <strcklnd@uw.edu>
2021-02-12 15:51:35 -0500

    fixed inoculate yeast library

--- yeast_display/libraries/yeastdisplayshows/source.rb
+++ yeast_display/libraries/yeastdisplayshows/source.rb
@@ -1,92 +1,74 @@
-
 needs 'Standard Libs/CommonInputOutputNames'
 
 module YeastDisplayShows
-    
-    include CommonInputOutputNames
+  include CommonInputOutputNames
 
-    def prepare_media(container_group, language)
-        flask_type = language[:flask_type]
-        media_volume = language[:media_volume]
-        
-        flask_type = flask_type.pluralize if container_group.length > 1
-        
-        show do
-          title "Prepare media"
-          warning "Work in the media bay for media preparation."
-          check "Grab #{container_group.length} #{flask_type}."
-          
-          out_ids = container_group.map { |op| op.output(OUTPUT_YEAST).item.id }.join(", ")
-          check "Label each flask or tube with the following ids: #{out_ids}"
-          
-          note "Add #{media_volume} of the following media into the #{flask_type}."
-          
-          table media_table(container_group)
-        end
+  def prepare_media(container_group, language)
+    media_volume = language[:media_volume]
+    output_items = container_group.map { |op| op.output(OUTPUT_YEAST).item }
+    flasks = language[:flask_type].pluralize(output_items.length)
+
+    show do
+      title 'Prepare media'
+
+      warning 'Work in the media bay for media preparation.'
+
+      note "Add #{media_volume} of the following media into the #{flasks}."
+      table media_table(container_group)
     end
-  
-    def innoculate_flasks(container_group, language)
-        passage_amount = language[:passage_amount]
-        
-        input_type = operations.first.input(INPUT_YEAST).name
-        plural_input_type = operations.length > 1 ? input_type.pluralize : input_type
-        language = language.merge({ input_type: input_type })
-        
-        show do
-          title "Inoculate #{plural_input_type}"
-      
-          note passage_amount % language
-          table transfer_table(container_group)
-        end
+  end
+
+  def innoculate_flasks(container_group, language)
+    n_flasks = container_group.length
+    input_type = container_group.first.input(INPUT_YEAST).name
+    language = language.merge({ input_type: input_type })
+
+    show do
+      title "Inoculate #{input_type.pluralize(n_flasks)}"
+
+      note language[:passage_amount] % language
+      table transfer_table(container_group)
     end
-    
-    def media_table(container_group)
-        this_table = container_group.start_table
-        this_table = this_table.custom_column(heading: 'Media') { |op| op.input("Media").sample.name }
-        
-        if container_group.all? { |op| media_volume(op) }
-            this_table = this_table.custom_column(heading: 'Volume (ml)') { |op| media_volume(op).round }
-        end
-        
-        this_table.output_item(OUTPUT_YEAST, checkable: true).end_table
+  end
+
+  def media_table(container_group)
+    this_table = container_group.start_table
+    this_table = this_table.custom_column(heading: 'Media') { |op| op.input('Media').sample.name }
+
+    if container_group.all? { |op| media_volume(op) }
+      this_table = this_table.custom_column(heading: 'Volume (ml)') { |op| media_volume(op).round }
     end
-    
-    def get_media(container_group)
-        media = container_group.map { |op| op.input("Media").sample.name }.first
-        
-        show do
-            title "Get media"
-            
-            check "Get a bottle of #{media}."
-        end
+
+    this_table.output_item(OUTPUT_YEAST, checkable: true).end_table
+  end
+
+  def transfer_table(container_group)
+    this_table = container_group.start_table.input_item(INPUT_YEAST)
+
+    if container_group.any? { |op| sample_tube_label(op.input(INPUT_YEAST).item) }
+      this_table = this_table.custom_column(heading: 'Tube label') do |op|
+        sample_tube_label(op.input(INPUT_YEAST).item)
+      end
     end
-    
-    def transfer_table(container_group)
-        this_table = container_group.start_table.input_item(INPUT_YEAST)
-        
-        if container_group.any? { |op| sample_tube_label(op.input(INPUT_YEAST).item) }
-            this_table = this_table.custom_column(heading: 'Tube label') { |op| sample_tube_label(op.input(INPUT_YEAST).item) }
-        end
-        
-        if container_group.all? { |op| transfer_volume(op) }
-            this_table = this_table.custom_column(heading: 'Volume (ml)') { |op| transfer_volume(op).round(1) }
-        end
-        
-        this_table.output_item(OUTPUT_YEAST, checkable: true).end_table
+
+    if container_group.all? { |op| transfer_volume(op) }
+      this_table = this_table.custom_column(heading: 'Volume (ml)') { |op| transfer_volume(op).round(1) }
     end
-    
-    def measure_culture_ods(ops)
-        show do
-            title 'Measure library culture densities.'
-            
-            note 'Use the Nanodrop to measure the density of each yeast culture, and record the OD600.'
-            warning 'Record the OD600 exactly as it is shown on the screen.'
-            
-            table ops.start_table
-                .input_item(INPUT_YEAST)
-                .get(:od, type: 'number', heading: 'OD600')
-                .end_table
-        end
+
+    this_table.output_item(OUTPUT_YEAST, checkable: true).end_table
+  end
+
+  def measure_culture_ods(ops)
+    show do
+      title 'Measure library culture densities.'
+
+      note 'Use the Nanodrop to measure the density of each yeast culture, and record the OD600.'
+      warning 'Record the OD600 exactly as it is shown on the screen.'
+
+      table ops.start_table
+               .input_item(INPUT_YEAST)
+               .get(:od, type: 'number', heading: 'OD600')
+               .end_table
     end
-  
+  end
 end
--- yeast_display/operation_types/innoculate_yeast_library/protocol.rb
+++ yeast_display/operation_types/innoculate_yeast_library/protocol.rb
@@ -2,56 +2,32 @@
 
 needs 'Yeast Display/YeastDisplayShows'
 needs 'Yeast Display/YeastDisplayHelper'
-needs 'Standard Libs/SortHelper'
 
 # Inoculate Yeast Library Protocol
 #
 # @author Devin Strickland <strcklnd@uw.edu>
 class Protocol
-  include SortHelper
   include YeastDisplayHelper
   include YeastDisplayShows
 
   INPUT_YEAST = 'Yeast Culture'
 
   def main
-    # DO NOT DELETE SORT BY ID
-    ops_sorted = sortByMultipleIO(operations, ['in'], [INPUT_YEAST], ['id'], ['item'])
-    operations = ops_sorted
-
     set_test_labels(operations.map { |op| op.input(INPUT_YEAST).item }) if debug
 
+    operations.retrieve.make
+
     group_ops_by_container.each do |container, container_group|
-      container_group.retrieve(only: [INPUT_YEAST]).make
       prepare_media_and_dilute(container, container_group)
     end
-
     return_to_incubator
 
-    input_items = operations.map { |op| op.input(INPUT_YEAST).item }
-
-    discards = input_items.reject { |i| i.object_type.name =~ /(plate|glycerol)/i }
-
-    mark_cultures_for_discard(discards)
-
-    glycerol_stocks = input_items.select { |i| i.object_type.name =~ /(Yeast Library Glycerol Stock)/i }
-    glycerol_stocks.map(&:mark_as_deleted)
-
-    show do
-      title 'Throw Away Glycerol Stocks'
-
-      note "Throw away all the <b>empty</b> glycerol stocks (#{glycerol_stocks.map(&:to_s).to_sentence})."
-    end
+    operations.each { |op| op.input(INPUT_YEAST).item.mark_as_deleted }
+    discard_deleted_inputs(operations: operations)
 
     operations.store
+    transfer_bin_numbers(operations: operations)
 
-    # DO NOT DELETE
-    operations.each do |op|
-      unless op.input(INPUT_YEAST).item.get(:bin).nil? # sorted sample
-        op.output(OUTPUT_YEAST).item.associate(:bin, op.input(INPUT_YEAST).item.get(:bin))
-      end
-    end
-
-    # library glycerol stocks need to be deleted
+    {}
   end
 end
