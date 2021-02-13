4b9e8e31bb00b4118f0b504df521b5d729f784f8
Devin Strickland <strcklnd@uw.edu>
2021-02-12 20:17:01 -0500

    fixed and cleaned up dilute yeast culture

--- yeast_display/operation_types/dilute_yeast_library/protocol.rb
+++ yeast_display/operation_types/dilute_yeast_library/protocol.rb
@@ -1,42 +1,27 @@
-# Devin Strickland
-# dvn.strcklnd@gmail.com
+# frozen_string_literal: true
 
-needs "Yeast Display/YeastDisplayShows"
-needs "Yeast Display/YeastDisplayHelper"
-needs "Standard Libs/Debug"
-needs "Standard Libs/SortHelper"
+needs 'Yeast Display/YeastDisplayHelper'
 
+# Dilute Yeast Library Protocol
+#
+# @author Devin Strickland <strcklnd@uw.edu>
 class Protocol
+  include YeastDisplayHelper
 
-    include YeastDisplayShows, YeastDisplayHelper, Debug, SortHelper
+  def main
+    operations.retrieve.make
 
-    def main
-        
-        # DO NOT DELETE 
-        ops_sorted=sortByMultipleIO(operations, ["in"], [INPUT_YEAST], ["id"], ["item"])
-        operations=ops_sorted
-        
-        operations.retrieve
-        
-        group_ops_by_container.each do |container, container_group|
-            container_group.make
-            prepare_media_and_dilute(container, container_group)
-        end
-        
-        return_to_incubator
-        
-        mark_cultures_for_discard(operations.map { |op| op.input(INPUT_YEAST).item })
-        
-        operations.store
-        
-        # DO NOT DELETE 
-        operations.each { |op|
-            if( !(op.input(INPUT_YEAST).item.get(:bin).nil?) ) # sorted sample
-                op.output(OUTPUT_YEAST).item.associate(:bin, op.input(INPUT_YEAST).item.get(:bin))
-            end
-        }
-        operations.store
-        
+    group_ops_by_container.each do |container, container_group|
+      prepare_media_and_dilute(container, container_group)
     end
+    return_to_incubator
 
+    operations.each { |op| op.input(INPUT_YEAST).item.mark_as_deleted }
+    discard_deleted_inputs(operations: operations)
+
+    operations.store
+    transfer_bin_numbers(operations: operations)
+
+    {}
+  end
 end
